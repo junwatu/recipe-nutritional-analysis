263b9e8fd96223900b54b07d8ddef1d44ecc1aee
Equan P. <equan.p@gmail.com>
2024-06-03 00:24:45 +0700

    update server code

--- app/server.js
+++ app/server.js
@@ -1,11 +1,13 @@
 import path from 'path'
 import axios from 'axios'
+import { URL } from 'url'
 import express from 'express'
 
 const app = express()
-const port = 3000
-
-const baseUrl = 'https://www.wolframcloud.com/obj/4a0af00b-e14e-4467-b292-6d0ca161ff65'
+// eslint-disable-next-line no-undef
+const apiUrl = new URL(process.env.VITE_API_URL)
+const hostname = apiUrl.hostname
+const port = apiUrl.port || 4000
 
 app.use(express.json())
 app.use(express.static(path.join(path.resolve(), 'public')))
@@ -21,7 +23,8 @@ app.post('/analyze', async (req, res) => {
 	}
 
 	const params = new URLSearchParams({ ingredients: ingredients.join('\n') }).toString()
-	const url = `${baseUrl}?${params}`
+	// eslint-disable-next-line no-undef
+	const url = `${process.env.WOLFRAM_CLOUD_API}?${params}`
 
 	try {
 		const response = await axios.get(url)
@@ -36,6 +39,6 @@ app.get('/nutritions', (req, res) => {
 	res.send('This route will handle nutrition information.')
 })
 
-app.listen(port, () => {
-	console.log(`Server is running at http://localhost:${port}`)
+app.listen(port, hostname, () => {
+	console.log(`Server is running at http://${hostname}:${port}`)
 })
