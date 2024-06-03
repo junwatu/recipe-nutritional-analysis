b1a0656e8d305b4426b40581f4d48e7b27a18dc8
Equan P. <equan.p@gmail.com>
2024-06-03 21:14:11 +0700

    using prompt directly

--- app/libs/ai.js
+++ app/libs/ai.js
@@ -7,31 +7,6 @@ const messages = [
 	},
 ];
 
-// eslint-disable-next-line no-unused-vars
-const cleanupDataPrompt = async (recipe) => {
-	const prompt = ` Cleanup this recipe so the ingredients are arranged line by line. remove any how-to. Shorten any long ingredient description separated by a comma. Answer with the recipe list only.
-	
-	Example:
-
-        600 g chicken drumsticks
-        2 cloves garlic, (minced)
-        1 tsp ginger, (minced)
-        1 shallot, finely chopped
-        4 tbsp soy sauce
-        4 tbsp oyster sauce
-        2 tbsp black pepper
-        1 tbsp white pepper
-
-    Recipe>\n
-	
-	${recipe}
- 
-	\nAnd use the list of ingredient results for nutrition analysis.
-	`
-
-	return prompt
-}
-
 const tools = [{
 	type: "function",
 	function: {
@@ -62,14 +37,33 @@ const openai = new OpenAI({
 export async function agent(userInput) {
 	messages.push({
 		role: "user",
-		content: `Analyze a list of ingredients: \n${userInput}`,
+		content: `Cleanup this recipe so the ingredients are arranged line by line.remove any how- to.Shorten any long ingredient description separated by a comma.Answer with the recipe list only.
+
+			Example:
+
+	600 g chicken drumsticks
+	2 cloves garlic, (minced)
+	1 tsp ginger, (minced)
+	1 shallot, finely chopped
+	4 tbsp soy sauce
+	4 tbsp oyster sauce
+	2 tbsp black pepper
+	1 tbsp white pepper
+
+	Recipe >\n
+	
+	${userInput}
+
+	\nAnd use the list of ingredient results for nutrition analysis.
+	`,
 	});
 	const response = await openai.chat.completions.create({
 		model: "gpt-4o",
 		messages: messages,
 		tools: tools,
 	});
-	console.log(response);
+
+	console.log(response.choices[0].message?.tool_calls[0].function);
 }
 
 export default openai;
