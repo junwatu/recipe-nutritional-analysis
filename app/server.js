4999dcb5cdd1c4e725cecbdee44c26b2f8125c41
Equan P. <equan.p@gmail.com>
2024-06-03 19:36:45 +0700

    import wolfram function

--- app/server.js
+++ app/server.js
@@ -1,20 +1,13 @@
 import path from 'path'
-import axios from 'axios'
 import { URL } from 'url'
 import express from 'express'
-import OpenAI from 'openai'
+import { analyzeIngredients } from './libs/wolfram'
 
 const app = express()
 // eslint-disable-next-line no-undef
 const apiURL = new URL(process.env.VITE_API_URL)
 const hostname = apiURL.hostname
 const port = apiURL.port || 4000
-
-const openai = new OpenAI({
-	// eslint-disable-next-line no-undef
-	apiKey: process.env.OPENAI_API_KEY,
-	dangerouslyAllowBrowser: true,
-});
 
 app.use(express.json())
 app.use(express.static(path.join(path.resolve(), 'public')))
@@ -26,22 +19,11 @@ app.get('/', (req, res) => {
 app.post('/analyze', async (req, res) => {
 	const { ingredients } = req.body
 
-	// check with OpenAI here and use function calling to Wolfram Cloud
-
-	if (!ingredients || !Array.isArray(ingredients)) {
-		return res.status(400).json({ error: 'Invalid ingredients format' })
-	}
-
-	const params = new URLSearchParams({ ingredients: ingredients.join('\n') }).toString()
-	// eslint-disable-next-line no-undef
-	const url = `${process.env.WOLFRAM_CLOUD_API}?${params}`
-
 	try {
-		const response = await axios.get(url)
-		res.json(response.data)
+		const data = await analyzeIngredients(ingredients)
+		res.json(data)
 	} catch (error) {
-		console.error('Error fetching recipe:', error)
-		res.status(500).json({ error: 'Internal Server Error' })
+		res.status(400).json({ error: error.message })
 	}
 })
 
