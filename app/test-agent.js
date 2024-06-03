10b12143e5c0e86c3f3289f9242cd7f97d4f3c77
Equan P. <equan.p@gmail.com>
2024-06-03 19:59:34 +0700

    add agent test sample

---
+++ app/test-agent.js
@@ -0,0 +1,29 @@
+import { agent } from "./libs/ai";
+
+
+const result = agent(`600 g chicken drumsticks
+
+2 cloves garlic
+	(minced)
+
+1 tsp ginger
+	(minced)
+
+1 shallot
+	(finely chopped)
+
+4 tbsp soy sauce
+
+4 tbsp oyster sauce
+
+2 tbsp black pepper
+
+1 tbsp white pepper
+
+2 tsp salt
+
+1 tbsp brown sugar
+
+vegetable oil`)
+
+console.log(result)
